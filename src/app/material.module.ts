1b5bdcbc30f133ca9448bf2387622969abbbd3f8
StefanoA1 <stefanoA1@users.noreply.github.com>
2018-06-18 02:04:17 +0200

    Adding material.module for re-usability

---
+++ src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatToolbarModule, 
+  MatCardModule, MatSpinner, MatFormField, MatInputModule, MatDialogModule, MatTableModule } from '@angular/material';
+
+const modules = [
+        CommonModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatDialogModule,
+        MatTableModule
+];
+
+@NgModule({
+imports: [...modules],
+exports: [...modules],
+})
+export class CustomMaterialModule { }
