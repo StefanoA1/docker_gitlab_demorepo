bf566a2ff239d36e533bc50b4cd82e1cd06e94f4
StefanoA1 <stefanoA1@users.noreply.github.com>
2018-06-22 01:26:38 +0200

    Api services

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from './datamodel/user';
-// import { Observable } from 'rxjs/Observable';
-// import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/catch';
-// import 'rxjs/add/observable/throw';
 
 const API_URL = environment.apiUrl;
 
@@ -21,22 +17,19 @@ export class ApiService {
   public getAllUsers() {
     return this.http.get<User[]>(API_URL + '/users');
   }
-  
-//  public getAllUsers(): Observable<User[]> {
-//  return this.http
-//    .get('http://10.41.176.204:8080/quiz-manager-webapp/quiz-manager' + '/users')
-//    .map(response => {
-//      const users = response.json();
-//      return users.map((user) => new User(user));
-//    })
-//    .catch(this.handleError);
-//  }
-/*
-  // API: POST /todos
-  public createTodo(todo: Todo) {
-    // will use this.http.post()
+
+
+  // API: POST /user
+  public createUser(user: User) {
+    return this.http.post<User>(API_URL, '/create');
   }
 
+ // API: POST /user
+  public authUser(user: User) {
+    return this.http.post<User>(API_URL, '/auth');
+  }
+  
+/*
   // API: GET /todos/:id
   public getTodoById(todoId: number) {
     // will use this.http.get()
@@ -52,9 +45,6 @@ export class ApiService {
     // will use this.http.delete()
   }
 */
-//  private handleError (error: Response | any) {
-//    console.error('ApiService::handleError', error);
-//    return Observable.throw(error);
-//  }
+
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../datamodel/user';
+
+const API_URL = environment.apiUrl;
 
 @Injectable()
-export class UserService {
+export class ApiService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+
+  }
+  // API: GET /users API_URL
+  public getAllUsers() {
+    return this.http.get<User[]>(API_URL + '/users');
+  }
+
+
+  // API: POST /user
+  public createUser(user: User) {
+    return this.http.post<User>(API_URL, '/create');
+  }
+
+ // API: POST /user
+  public authUser(user: User) {
+    return this.http.post<User>(API_URL, '/auth');
+  }
 
 }
